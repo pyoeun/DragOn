c3c1e313784a6a90a8aaaa8bc6562ac9d8af6b88
PyoDev <eunpyo0621@gmail.com>
2024-01-16 00:08:52 +0900

    .

--- DragOn/Assets/Lee/Ready/MainSingleton.cs
+++ DragOn/Assets/Lee/Ready/MainSingleton.cs
@@ -7,4 +7,34 @@ public class MainSingleton : MonoBehaviour
     public static int dragon;
     public static int skill;
     public static int bullet;
+    private static MainSingleton instance = null;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public static MainSingleton Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+    private void Update()
+    {
+        Debug.Log($"{dragon}, {skill}, {bullet}");
+    }
 }
