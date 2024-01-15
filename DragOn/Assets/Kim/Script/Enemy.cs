898c817cbf70cdd7372198557d0063bb58eb2a1c
Jin <128784519+Nevil0713@users.noreply.github.com>
2024-01-15 12:13:29 +0900

    .

---
+++ DragOn/Assets/Kim/Script/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int health = 100;
+    float speed = 3;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            health -= 30;
+            if(health <= 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        if (collision.gameObject.tag == "Finish")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- DragOn/Assets/Kim/Script/SpawnManager.cs
+++ DragOn/Assets/Kim/Script/SpawnManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public string[] EnemyObjs;
+    public GameObject Enemy;
+
+    void SpawnEnemy()
+    {
+        float randomY = Random.RandomRange(-4f, 4f);
+        GameObject enemy = (GameObject)Instantiate(Enemy, new Vector3(10, randomY, 0f), Quaternion.identity);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnEnemy", 2, 2);
     }
 
     // Update is called once per frame
