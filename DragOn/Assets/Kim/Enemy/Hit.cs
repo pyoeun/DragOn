19f57ca277d85f5f42ea90041d681a86619c87eb
Jin <128784519+Nevil0713@users.noreply.github.com>
2024-01-17 18:00:12 +0900

    .

--- DragOn/Assets/Kim/Enemy/Hit.cs
+++ DragOn/Assets/Kim/Enemy/Hit.cs
@@ -9,7 +9,7 @@ public class Hit : MonoBehaviour
     {
         if (collision.gameObject.tag == "Bullet")
         {
-            Health -= collision.gameObject.GetComponent<Bullet>().damage;
+            Health -= collision.gameObject.GetComponent<Bullet>().Damage;
             if (Health <= 0)
             {
                 Destroy(gameObject);
